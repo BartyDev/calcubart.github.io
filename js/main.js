07a77ac9e271735634f8ae697a2aca790fdfd43c
barty91 <elvis.velasquez91@gmail.com>
2022-08-18 00:48:18 -0400

    cambios :v

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
 alert("Bienvenido a calculadora BartMod-1");
 
 let signo = prompt("Ingresa la operacion a realizar ! \n 1 - Para sumar \n 2 - Para restar \n 3 - Para multiplicar \n 4 - Para dividir"); 
+
+while ((signo === "") || (signo === "0") || (signo >= "5")) {
+    alert("No se cumplen las condiciones, VUELVA A INTENTARLO")
+    if (numero1 === "0") {
+       alert("No se cumplen las condiciones, VUELVA A INTENTARLO") 
+    }else if (numero2 === "0")
+        alert("No se cumplen las condiciones, VUELVA A INTENTARLO") 
+    
+    break;
+}
+
+
 let numero1 = parseInt(prompt("Ingrese un número :"));
 let numero2 = parseInt(prompt("Ingrese otro número :"));
 
 function calcular() {
     if (signo === "1") {
-        alert("Resultado : " + (numero1 + numero2));
+        alert("Resultado : " + (numero1 + numero2) + "  ✔️");
     } else if (signo === "2") {
-        alert("Resultado : " + (numero1 - numero2));
-    }else if (signo === "3") {
-        alert("Resultado : " + (numero1 * numero2));
-    }else if (signo === "4") {
-        alert("Resultado : " + (numero1 / numero2));
-    } else {
-        alert("Ingresaste un signo incorrecto :v")
-    }
+        alert("Resultado : " + (numero1 - numero2) + "  ✔️");
+    } else if (signo === "3") {
+        alert("Resultado : " + (numero1 * numero2) + "  ✔️");
+    } else{
+        alert("Resultado : " + (numero1 / numero2) + "  ✔️");
+    } 
 }
+
+
 
 calcular();
 
